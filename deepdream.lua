ffae6085c81a68cbe78dca4a2e556ee01c6c7f6a
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2016-12-26 00:07:03 -0500

    Added more parameters

--- deepdream.lua
+++ deepdream.lua
@@ -4,7 +4,7 @@ require 'image'
 local cuda = true
 local n_iter = 100
 local n_ectave = 4
-local n_end_layer = 39
+local n_end_layer = 40
 
 torch.setdefaulttensortype('torch.FloatTensor')
 --net = torch.load('./GoogLeNet.t7')
